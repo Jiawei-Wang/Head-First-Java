fde405b9f0f343daa338085cee3d35ec9d13359a
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-05-12 22:25:51 -0400

    Upload SimpleDotComTestDrive

---
+++ Chapter 05/v2.0/SimpleDotComTestDrive.java
@@ -0,0 +1,55 @@
+class SimpleDotCom {
+
+    int[] locationCells;
+    int numOfHits = 0;
+
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
+    }
+
+    public String checkYourself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits ++;
+                break;
+            }
+        }
+
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result;
+    }
+}
+
+
+// code for testing before implementation
+public class SimpleDotComTestDrive {
+    public static void main (String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+
+        // initialize an object of SimpleDotCom
+        SimpleDotCom theDotCom = new SimpleDotCom();
+
+        int randomNum = (int) (Math.random()*5);
+        int[] locations = {randomNum, randomNum+1, randomNum+2};
+
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+
+        while(isAlive == true) {
+            String guess = helper.getUserInput("enter a number");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took "+numOfGuesses+" guesses");
+            }
+        }
+    }
+}
