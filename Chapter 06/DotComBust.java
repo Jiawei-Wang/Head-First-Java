aa288e3e9ce7289f78946a17edf1ae912e40611e
Jiawei Wang <wangjiawei_med@hotmail.com>
2020-05-16 22:09:04 -0400

    Start DotComBust

---
+++ Chapter 06/DotComBust.java
@@ -0,0 +1,9 @@
+// read files
+import java.util.*;
+
+public class DotComBust {
+    // GameHelper instance
+    private GameHelper helper = new GameHelper();
+    // new list for DotCom instances
+    
+}
